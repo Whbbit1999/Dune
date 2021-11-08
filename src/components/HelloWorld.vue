20a407b0d3b9abba4330f0988cff1ba724cd80d8
whbbit <luckyboy_dong@sina.com>
2021-11-08 21:14:14 +0800

    feat: 添加boot-strap-icon

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,9 +1,9 @@
 <script setup lang="ts">
-import { ref } from 'vue'
+import { ref } from "vue";
 
-defineProps<{ msg: string }>()
+defineProps<{ msg: string }>();
 
-const count = ref(0)
+const count = ref(0);
 </script>
 
 <template>
@@ -31,6 +31,7 @@ const count = ref(0)
     Edit
     <code>components/HelloWorld.vue</code> to test hot module replacement.
   </p>
+  <BIconArrow90degDown />
 </template>
 
 <style scoped>
